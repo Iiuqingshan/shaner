3438abe4e3dea706b3556fb7fb2edb6d3c104ef1
qingshan <liuqingshan86@gmail.com>
2024-08-28 10:38:53 +0800

    first commit

---
+++ lib/shaner.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shaner/version"
+
+module Shaner
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shaner/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Shaner
+  VERSION = "0.1.0"
+end
